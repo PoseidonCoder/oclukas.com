542c5b7f5b4cab84be785c7e3b00d2c6d0dbeaed
marchellodev <marchellodev@gmail.com>
2021-05-10 23:02:05 +0300

    Typo & grammar in about.svelte :)

--- src/about.svelte
+++ src/about.svelte
@@ -4,14 +4,14 @@
     <div class="block">
         <h2>Tech</h2>
         <p>
-            I love everything to do with technology; from hardware to software.
+            I love everything related to technology; from hardware to software.
             I believe that computers were mans greatest invention.
             I've dabbled with Linux, a multitude of programming languages, open-source, and Rasberry Pi.
         </p>
 
         <h2>School</h2>
         <p>
-            I'm currently a student at a school that hasn't hadeled distance learning very well.
+            I'm currently a student at a school that hasn't handled distanced learning very well.
             However, one of my greatest weaknesses at this school is procrastination.
         </p>
     </div>
